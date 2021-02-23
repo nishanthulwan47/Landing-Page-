6e1be8aa4b2d87a2d9ddf0bf6e5b372816e46c1c
Nishant Hulwan <nishanthulwan47@gmail.com>
2021-02-23 16:11:07 -0500

    first commit

--- js/app.js
+++ js/app.js
@@ -17,7 +17,7 @@
  * Define Global Variables
  *  
 */
-    const navbarList = document.getElementById("#navbar_list");
+    const navbarList = document.getElementById("navbar_list");
     const sections = document.getElementsByTagName("section");
 
 /**
@@ -25,7 +25,26 @@
  * Start Helper Functions
  * 
 */
-    
+    //Determine if an element is in viewport
+
+    //Helper functions
+    function isInViewport(element) {
+
+        const viewport = element.getBoundingClientRect();
+        return (
+            viewport.top >= 0 &&
+            viewport.left >= 0 &&
+            viewport.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+            viewport.right <= (window.innerWidth || document.documentElement.clientWidth)
+        );
+            
+        
+    };
+
+    // var h1 = document.querySelector('h1');
+    // var bounding = h1.getBoundingClientRect();
+
+    // console.log(bounding);
     
 
 
@@ -45,19 +64,38 @@ function buildNav() {
         //create the <a> element
         let navItemLink = document.createElement('a');
         navItemLink.className = 'menu__link';
+
+        navItemLink.dataset["section"] = section.id;
+
         //use the section data-nav to set the navItem title
         navItemLink.textContent = section.dataset.nav;
         //append the link to the navItem
         navItem.appendChild(navItemLink);
         //append the link to navbar
         navbarList.appendChild(navItem);
+
+        
     };
 };
 
-buildNav();
+
 
 // Add class 'active' to section when near top of viewport
 
+function classActive() {
+    for (section of sections) {
+        if (isInViewport(section)) {
+            console.log('In the viewport!');
+            section.classList.add("your-active-class");
+        } else {
+            section.classList.remove("your-active-class");
+            console.log('Not in viewport.. whomp whomp');
+        }
+    }
+};
+
+
+buildNav();
 
 // Scroll to anchor ID using scrollTO event
 
